c66dfe25b03bbc0714601e630fbe9be8016dd33e
funnut <195337009+funnut@users.noreply.github.com>
2025-04-15 02:14:08 +0200

    nowey screenshot

--- test.py
+++
@@ -1,7 +0,0 @@
-from random import choice, randrange
-
-notesfilename = '/data/data/com.termux/files/home/notatnik/notatnik.txt'
-
-with open (notesfilename, 'r') as plik:
-    do_wyswietlenia = plik.readlines()
-    print (choice(do_wyswietlenia))
