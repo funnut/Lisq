69cc86efe0e9fb36f31c7881942dfc4ea5edad8d
funnut <195337009+funnut@users.noreply.github.com>
2025-05-01 21:40:08 +0200

    File(s) updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lisq",
-    version="0.1.0",
+    version="0.11.0",
     description="Single file note-taking app that work with .txt files",
     url="https://github.com/funnut/Lisq.git",
     author="funnut",
@@ -17,7 +17,7 @@ setup(
     ],
     entry_points={
     'console_scripts': [
-        'lisq=lisq.lisq:glowna_funkcja',
+        'lisq=lisq.lisq',
     ],
     },
     include_package_data=True,
