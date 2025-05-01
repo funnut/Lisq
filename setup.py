66a20750f6432bb491a0b2244f579cbe4609ee55
funnut <195337009+funnut@users.noreply.github.com>
2025-05-01 21:04:44 +0200

    File(s) updated

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="lisq",
-    version="0.1.0",
+    version="2025.05.01",
     description="Single file note-taking app that work with .txt files",
+    url="https://github.com/funnut/Lisq.git"
     author="funnut",
     author_email="twoj@email.com",
+    project_urls={
+        "Bug Trucker": "https://github.com/funnut/Lisq/issues",
+        "Source Code": "https://github.com/funnut/Lisq.git",
+    },
     packages=find_packages(),
     install_requires=[
         "requests",  # przykładowa zależność
