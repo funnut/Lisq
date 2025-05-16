0f6563ecf1c2c4134a4c53deff925c8a3d9f57cc
funnut <195337009+funnut@users.noreply.github.com>
2025-05-16 17:38:43 +0200

    File(s) updated

--- tests/rebuild/rebuild.py
+++ tests/rebuild/rebuild.py
@@ -1,15 +1,14 @@
 import logging
 import readline
-import os
+import os, sys
 from pathlib import Path
 from datetime import date
 from random import choice
 
-
 # Konfiguracja log - logging
 logging.basicConfig(
     level=logging.DEBUG, # DEBUG, INFO, WARNING, ERROR, CRITICAL
-    filename="debug.log",  # rm by logować na konsolę
+#    filename="debug.log",  # rm by logować na konsolę
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
@@ -19,6 +18,7 @@ histfile = script_dir / "_history"
 if histfile.exists():
     readline.read_history_file(histfile)
 readline.set_history_length(100)
+
 
 def NOTES_PATH(): # - pathlib, os
     path = Path("~/_lisq/tests/rebuild/").expanduser() / "notes.txt"
@@ -33,32 +33,52 @@ def clear(args): # - os
     terminal_hight = os.get_terminal_size().lines
     print("\n"*terminal_hight*2)
 
+def reiterate(args=None):
+    logging.info("Start reiterate()")
+    try:
+        with open(NOTES_PATH(), "r", encoding="utf-8") as f:
+            lines = f.readlines()
+            id_ = 0
+            new_lines = []
+            for line in lines:
+                id_ += 1
+                parts = line.strip().split()
+                if not parts:
+                    continue
+                new_id = f"i{str(id_).zfill(3)}"
+                new_line = f"{new_id} {' '.join(parts[1:])}\n"
+                new_lines.append(new_line)
+            with open(NOTES_PATH(),"w",encoding="utf-8") as f:
+                f.writelines(new_lines)
+            logging.info(f"Zaktualizowano identyfikatory dla {id_} linii.")
+    except FileNotFoundError:
+        logging.error("FileNotFoundError w reiterate()")
+        print ("Nie znaleziono notatnika")
+
 def delete(args):
     """Usuwanie notatek."""
-    logging.info(f"Start delete({args})")
-    arg = args[0] if args else None
+    logging.info(f"Start delete({args}){len(args)}")
     try:
-        logging.debug(f"arg: {arg}")
-        while not arg:
-            arg = input("  - ").strip()
-            if ' ' in arg:
+        while not args[0]: # delete
+            args = input(" - ").strip()
+            if ' ' in args:
                 print ("Polecenie musi być pojedynczym słowem!")
-                arg = None
-            if arg in ["q",""]:
+                args = None
+            if args in ["q",""]:
                 return
 
         with open(NOTES_PATH(),"r",encoding="utf-8") as f:
             lines = f.readlines()
 
-        if arg == "all":
+        if args[0] == "all": # all
             yesno = input("Czy usunąć wszystkie notatki? (y/n): ").strip().lower()
-            if yesno in ["yes","y"]:
+            if yesno in ["yes","y",""]:
                 open(NOTES_PATH(),"w",encoding="utf-8").close()
                 print ("Wszystkie notatki zostały usunięte.")
             else:
                 print ("Anulowano.")
 
-        elif arg in ["last","l"]:
+        elif args[0] in ["last","l"]: # last
             yesno = input("Czy usunąć ostatnią notatkę? (y/n): ").strip().lower()
             if yesno in ["y",""]:
                 with open(NOTES_PATH(),"w",encoding="utf-8") as f:
@@ -66,14 +86,19 @@ def delete(args):
                 print ("Usunięto ostatnią notatkę.")
             else:
                 print ("Anulowano.")
-        else:
-            new_lines = [line for line in lines if arg not in line]
+
+        else: # [id]
+            new_lines = [line for line in lines if not any(el in line for el in args)]
+            found = [arg for arg in args if any(arg in line for line in lines)]
             number = len(lines)-len(new_lines)
+            if not all(any(arg in line for line in lines) for arg in args):
+                print ("Nie wszystkie elementy zostały znalezione.")
             if number > 0:
-                yesno = input(f"Czy usunąć {number} notatek zawierających '{arg}'? (y/n): ").strip().lower()
+                yesno = input(f"Czy usunąć {number} notatki zawierające {found}? (y/n): ").strip().lower()
                 if yesno in ["yes","y",""]:
                     with open(NOTES_PATH(),"w",encoding="utf-8") as f:
                         f.writelines(new_lines)
+                    reiterate()
                     print ("Usunięto notatkę/i.")
                 else:
                     print ("Anulowano.")
@@ -83,7 +108,6 @@ def delete(args):
     except FileNotFoundError:
         logging.error("FileNotFoundError w read_file()")
         print ("Nie znaleziono notatnika.")
-
 
 def read_file(args):
     """Odczyt pliku notatek""" # - random, os
@@ -115,18 +139,17 @@ def read_file(args):
             parts = line.split()
             date_ = "-".join(parts[1].split("-")[1:])
             print (f"{parts[0]} {date_} {" ".join(parts[2:]).strip()}")
-#        print (f"\nZnaleziono {len(to_show)} pasujących elementów.")
+        print (f"\nZnaleziono {len(to_show)} pasujących elementów.")
 
     except FileNotFoundError:
         logging.error("FileNotFoundError w read_file()")
         print ("Nie znaleziono notatnika.")
 
-
 def write_file(args): # - datetime
     """Zapisywanie notatek do pliku w ustalonym formacie."""
     logging.info(f"Start write_file({args})")
     if not args:
-        args = input("  : ").strip().split()
+        args = input(" : ").strip().split()
         if not args:
             return
     try:
@@ -152,36 +175,39 @@ def echo(text):
     print(text)
 
 def _test(args):
-    logging.info(f"Start _test({args})")
-    print(NOTES_PATH())
-    print("Hello sir!")
+    #print("type(args):",type(args))
     print("args:",args)
-    print("bool(args):",bool(args))
-    print("type(args):",type(args))
-
-    tablica = ["abc","1","3","cb a", " al i on"]
-    print("tablica=",tablica)
-    s = ":-1" # args[0]
-    print("tablica=",eval(f"tablica[{s}]"))
+    with open(NOTES_PATH(), "r") as f:
+        lines = f.readlines()
+#    tablica = ["i001","i002","i003","i004", "i005"]
+    print (lines,"\n\n")
+    czy_any = any(element in line for element in args for line in lines)
+    print (czy_any)
+    czy_all = all(any(arg in line for line in lines) for arg in args)
+    print (czy_all)
+    #s = args[0]
+    #print("tablica=",eval(f"tablica[{s}]"))
 
 # dispatch table
 commands = {
+    "edit": lambda args: os.system(f"nano {NOTES_PATH()}"),
     "echo": lambda args: echo(" ".join(args)),
     "test": _test,
     "add": write_file,
     "show": read_file,
     "s": read_file,
     "del": delete,
+    "delete": delete,
     "c": clear,
+    "reiterate": reiterate,
 }
 
-def main():
+def main(): # - sys
     logging.info("START FUNKCJI main()")
     while True:
         logging.debug("START GŁÓWNEJ PĘTLI")
         try:
-            raw = input(" > ").strip()
-
+            raw = input("> ").strip()
             if not raw:
                 write_file(args=None)
                 continue
@@ -218,3 +244,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
